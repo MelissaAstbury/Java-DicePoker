01a6be0d07f427dbbd87b16dca36c982c98134ff
Melissa Astbury <melissa.astbury@footasylum.com>
2023-08-03 15:06:16 +0200

    refactor

--- src/dicePoker/Computer.java
+++ src/dicePoker/Computer.java
@@ -1,6 +1,5 @@
 package dicePoker;
 
-import java.util.ArrayList;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class Computer {
@@ -10,17 +9,11 @@ public class Computer {
 	public int diceTwo;
 	
 	// Method for the computer to randomly 'roll' two dice so two numbers can be generated for that round
-	//public ArrayList<Integer> RollTheDice(){
 	public void RollTheDice(){
-		//ArrayList<Integer> diceNumbers = new ArrayList<Integer>();
 		int min = 1;
 		int max = 6;
 		
 		diceOne = ThreadLocalRandom.current().nextInt(min, max + 1);
 		diceTwo = ThreadLocalRandom.current().nextInt(min, max + 1);
-		//diceNumbers.add(diceOne);
-		//diceNumbers.add(diceTwo);
-		
-		//return diceNumbers;
 	}
 }
